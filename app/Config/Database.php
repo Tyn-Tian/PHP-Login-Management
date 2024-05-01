57b472535d7a5496c9736925ea923357d4a55441
Christian <kristian2015ok@gmail.com>
2024-05-01 14:31:53 +0700

    Add: Database Connection

---
+++ app/Config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LoginManagement\Config;
+
+class Database
+{
+    private static ?\PDO $pdo = null;
+
+    public static function getConnection(string $env = "test") 
+    {
+        if (self::$pdo == null) {
+            require_once __DIR__ . "/../../config/database.php";
+            $config = getDatabaseConfig();
+            self::$pdo = new \PDO(
+                $config["database"][$env]["url"],
+                $config["database"][$env]["username"],
+                $config["database"][$env]["password"]
+            );
+        }
+
+        return self::$pdo;
+    }
+}
