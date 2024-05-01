a9b1f0b0415d8aadd3bf1501c217c2ee9f93c531
Christian <kristian2015ok@gmail.com>
2024-05-01 15:07:46 +0700

    Add: View Template

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,42 +8,8 @@ class HomeController
 {
     function index(): void 
     {
-        $model = [
-            "title" => "Belajar PHP MVC",
-            "content" => "Selamat belajar PHP MVC"
-        ];
-        
-        View::render("Home/index", $model);
-    }
-
-    function hello(): void 
-    {
-        echo "HomeController.hello()";
-    }
-
-    function world(): void 
-    {
-        echo "HomeController.world()";
-    }
-
-    function about(): void 
-    {
-        echo "Author: Christian";
-    }
-
-    function login(): void
-    {
-        $request = [
-            "username" => $_POST['username'],
-            "password" => $_POST['password']
-        ];
-
-        $user = [
-
-        ];
-
-        $response = [
-            "message" => "Login Sukses"
-        ];
+        View::render("Home/index", [
+            "title" => "PHP Login Management"
+        ]);
     }
 }
