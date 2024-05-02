7b49ea4a019671d3a77b08a09dc1be89ad89edda
Christian <kristian2015ok@gmail.com>
2024-05-02 09:33:56 +0700

    Add: User Registration Controller

--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use LoginManagement\App\Router;
+use LoginManagement\Config\Database;
 use LoginManagement\Controller\HomeController;
+use LoginManagement\Controller\UserController;
 
+Database::getConnection("prod");
+
+// Home
 Router::add("GET", "/", HomeController::class, "index", []);
+
+// Register
+Router::add("GET", "/users/register", UserController::class, "register", []);
+Router::add("POST", "/users/register", UserController::class, "postRegister", []);
 
 Router::run();
