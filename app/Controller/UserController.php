b5919d9d387d41bca48443e0a1500f5474027bd9
Christian <kristian2015ok@gmail.com>
2024-05-02 11:13:23 +0700

    Add: User Login Controller

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace LoginManagement\Controller;
 use LoginManagement\App\View;
 use LoginManagement\Config\Database;
 use LoginManagement\Exception\ValidationException;
+use LoginManagement\Model\UserLoginRequest;
 use LoginManagement\Model\UserRegisterRequest;
 use LoginManagement\Repository\UserRepository;
 use LoginManagement\Service\UserService;
@@ -40,6 +41,30 @@ class UserController
         } catch (ValidationException $exception) {
             View::render("Users/register", [
                 "title" => "Register New User",
+                "error" => $exception->getMessage()
+            ]);
+        }
+    }
+
+    public function login(): void
+    {
+        View::render("Users/login", [
+            "title" => "Login User"
+        ]);
+    }
+
+    public function postLogin(): void
+    {
+        $request = new UserLoginRequest();
+        $request->id = $_POST["id"];
+        $request->password = $_POST["password"];
+
+        try {
+            $response = $this->userService->login($request);
+            View::redirect("/");
+        } catch (ValidationException $exception) {
+            View::render("Users/login", [
+                "title" => "Login User",
                 "error" => $exception->getMessage()
             ]);
         }
