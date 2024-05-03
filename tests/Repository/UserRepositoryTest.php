e90e1c799a6a46e4dee3fca34915313289a181f1
Christian <kristian2015ok@gmail.com>
2024-05-03 09:55:19 +0700

    Test: User Profile Update Repository

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -9,9 +9,13 @@ use PHPUnit\Framework\TestCase;
 class UserRepositoryTest extends TestCase
 {
     private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     protected function setUp(): void
     {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();
+
         $this->userRepository = new UserRepository(Database::getConnection());
         $this->userRepository->deleteAll();
     }
@@ -34,5 +38,23 @@ class UserRepositoryTest extends TestCase
     {
         $result = $this->userRepository->findById("empty");
         self::assertNull($result);
+    }
+
+    public function testUpdate()
+    {
+        $user = new User();
+        $user->id = "testId";
+        $user->name = "testName";
+        $user->password = "testPassword";
+        $this->userRepository->save($user);
+
+        $user->name = "testNameChange";
+        $this->userRepository->update($user);
+        
+        $result = $this->userRepository->findById($user->id);
+
+        self::assertEquals($user->id, $result->id);
+        self::assertEquals($user->name, $result->name);
+        self::assertEquals($user->password, $result->password);
     }
 }
